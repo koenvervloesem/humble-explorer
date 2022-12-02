c4688c4f848856c8cd6231245d0d28b7e5462cd4
Koen Vervloesem <koen@vervloesem.eu>
2022-12-02 09:05:30 +0100

    Color Bluetooth addresses and timestamps

---
+++ src/humble_explorer/utils.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+__author__ = "Koen Vervloesem"
+__copyright__ = "Koen Vervloesem"
+__license__ = "MIT"
+
+permutation_table = list(range(256))
+shuffle(permutation_table)
+
+
+def hash8(message: str) -> int:
+    """Compute an 8-bit hash from the message with Pearson hashing."""
+    # Source: https://en.wikipedia.org/wiki/Pearson_hashing
+    hash_value = len(message) % 256
+    for i in message:
+        hash_value = permutation_table[hash_value ^ ord(i)]
+    return hash_value
--- tests/test_renderables.py
+++ tests/test_renderables.py
@@ -1,10 +1,21 @@
 from rich.text import Text
 
-from humble_explorer.renderables import RSSI
+from humble_explorer.renderables import RSSI, Now
 
 __author__ = "Koen Vervloesem"
 __copyright__ = "Koen Vervloesem"
 __license__ = "MIT"
+
+
+def test_now():
+    """Test Now class."""
+    now1 = Now()
+    now2 = Now()
+    # make now1 and now2 times within the same second
+    while now1.time.split(".")[0] != now2.time.split(".")[0]:
+        now1 = Now()
+
+    assert now1.style == now2.style
 
 
 def test_rssi():
