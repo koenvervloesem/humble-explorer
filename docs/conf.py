c21762a8eaf3cc0819c985d9e6b219bb4532d047
Koen Vervloesem <koen@vervloesem.eu>
2023-01-24 10:45:43 +0100

    Use furo theme for documentation (#46)

--- docs/conf.py
+++ docs/conf.py
@@ -153,14 +153,13 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
+    "navigation_with_keys": True,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
