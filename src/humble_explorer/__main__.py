b4b5c2c07c0f7b461624963c12c107795c8912f6
Koen Vervloesem <koen@vervloesem.eu>
2023-03-22 23:13:49 +0100

    Show available Bluetooth adapters (#53)

--- src/humble_explorer/__main__.py
+++ src/humble_explorer/__main__.py
@@ -1,6 +1,7 @@
 """Main entry point for HumBLE Explorer."""
 from __future__ import annotations
 
+import asyncio
 import sys
 from argparse import ArgumentParser, Namespace
 
@@ -12,21 +13,15 @@ __copyright__ = "Koen Vervloesem"
 __license__ = "MIT"
 
 
-# ---- CLI ----
-# The functions defined in this section are wrappers around the main Python
-# API allowing them to be called directly from the terminal as a CLI
-# executable/script.
-
-
-def run() -> None:
+async def run() -> None:
     """Calls :func:`main` passing the CLI arguments extracted from `sys.argv`.
 
     This function can be used as entry point to create console scripts with setuptools.
     """
-    main(sys.argv[1:])
+    await main(sys.argv[1:])
 
 
-def main(args: list[str]) -> None:
+async def main(args: list[str]) -> None:
     """Wrapper allowing the BLE scanner app to be called on the command line.
 
     This wrapper accepts string arguments.
@@ -35,12 +30,12 @@ def main(args: list[str]) -> None:
       args (list[str]): command line parameters as list of strings
           (for example  ``["--scanning-mode", "passive"]``).
     """
-    cli_args = parse_args(args)
+    cli_args = await parse_args(args)
     app = BLEScannerApp(cli_args=cli_args)
-    app.run()
+    await app.run_async()
 
 
-def parse_args(args: list[str]) -> Namespace:
+async def parse_args(args: list[str]) -> Namespace:
     """Parse command line parameters.
 
     Args:
@@ -56,9 +51,31 @@ def parse_args(args: list[str]) -> Namespace:
         action="version",
         version=f"humble-explorer {__version__}",
     )
-    parser.add_argument(
-        "-a", "--adapter", help="Bluetooth adapter (e.g. hci1 on Linux)", type=str
-    )
+
+    if sys.version_info[:2] >= (3, 9):
+        from bluetooth_adapters import get_adapters
+
+        bluetooth_adapters = get_adapters()
+        await bluetooth_adapters.refresh()
+        adapters = sorted(bluetooth_adapters.adapters.keys())
+        default_adapter = bluetooth_adapters.default_adapter
+
+        parser.add_argument(
+            "-a",
+            "--adapter",
+            help=f"Bluetooth adapter: (default: {default_adapter})",
+            type=str,
+            default=default_adapter,
+            choices=tuple(adapters),
+        )
+    else:
+        parser.add_argument(
+            "-a",
+            "--adapter",
+            help="Bluetooth adapter: (e.g. hci1 on Linux)",
+            type=str,
+        )
+
     parser.add_argument(
         "-s",
         "--scanning-mode",
@@ -83,4 +100,4 @@ if __name__ == "__main__":
     #    being executed in the case someone imports this file instead of
     #    executing it as a script.
     #    https://docs.python.org/3/library/__main__.html
-    run()
+    asyncio.run(run())
